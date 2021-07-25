6a8bd052b192289cbbc488654d3bc65be12e4d66
unknown <sanketpatil68@gmail.com>
2021-07-25 15:22:01 +0530

    added dom example

---
+++ app.js
@@ -0,0 +1,5 @@
+function add(){
+var num1 = parseInt (document.getElementById('num1').value);
+var num2 = parseInt (document.getElementById('num2').value);
+document.getElementById('res').innerHTML = '<h1>'+ num1+num2 + '</h1>'
+}
